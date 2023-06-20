82363eb9816b639d804edbf34aed7441cb7e5f97
mookdook94 <134980150+mookdook94@users.noreply.github.com>
2023-06-20 16:38:34 -0700

    Project week Day 1

---
+++ main.js
@@ -0,0 +1,143 @@
+
+
+//MVC-
+//Data That Needs To Be Remembered - Players , Whos Turn, Tally points(state variables)
+// 
+// ----------------------------Constants------------------------------ (fixed variables)
+
+
+
+const cards = [ 'A', 'A', 'B', 'B'] ;
+    
+
+
+
+//-----------------------------State Variables-----------------------------------
+
+let startCards; // total cards in the game 
+let flippedCards;// when player p1(player 1) or C(computer) have clicked the cards to flip
+let matchedCards; // after fliiping the cards in matching them
+let isGameOver; // when there are no cards on the screen . Game is Over!
+
+
+
+
+//----------------------------cache---------------------------------------------
+
+
+const startButton = document.querySelector('#start-button');
+
+const cardEls = document.querySelectorAll('.card');
+
+
+
+//---------------------------------Event Listeners----------------------------
+
+startButton.addEventListener('click', renderStartCards )
+
+
+//-----------------------------------init()-------------------------------------
+init();//have to ALWAYS call the init function it initialize all state variables.
+// This function make sure all variables have a starting point
+
+
+//-------------------------------------Function---------------------------------------
+
+
+
+function init() {  // initialize all state then call render
+
+flippedCards = 0; // no cards should be flipped when game start
+matchedCards = 0; // no matched cards when game start
+isGameOver = null; // no winner when game start
+
+}
+//for each / loop through each card element and add the text of each array to each one 
+function renderStartCards() {
+
+   cardEls.forEach(function(cardEl, index) {
+       const cardValue = cards[index];
+       cardEl.textContent =cardValue;
+    });  
+}
+
+function renderstartButton () {
+    if (startButton) 
+}
+
+
+
+
+
+
+
+
+  
+  
+  
+ 
+   
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //using my STATE Variables in this section to Render or Display elements on the screen when needed
+// function renderGame(){
+// if(gameState ==="playing") {
+//     //update game elements
+
+// } else if (gameState === "gameover"){
+//     //display game over sceen
+// }
+
